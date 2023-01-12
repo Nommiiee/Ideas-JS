c9306d3dcd08186206a2e8b6fb4ae2d2c666abb0
Nommiiee <kunalkashyp3@gmail.com>
2023-01-13 01:16:13 +0530

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dataSet = require("./models/dataSet.js");
+const ideaSchema = require("./models/dataSet.js");
 const app = express();
 const port = 3000;
 const ideasData = require("./src/ideas.js");
@@ -27,4 +27,61 @@ const initialise = async function () {
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("./dist/index.html"));
+app.use(express.static("./dist"));
+
+app.get("/idea", async (req, res) => {
+  try {
+    const idea = await getRandomIdea();
+    res.json(idea);
+  } catch (error) {
+    console.log(error);
+    res.send("Something Went Wrong, Please Try Again");
+  }
+});
+
+app.get("/refresh", async (req, res) => {
+  try {
+    await deleteIdeas();
+    await putIdeas();
+    res.send("done");
+  } catch (err) {
+    console.log(err);
+    res.send("Something Went Wrong, Please Try Again");
+  }
+});
+
+function titleCase(str) {
+  str = str.toLowerCase().split(" ");
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+  }
+  return str.join(" ");
+}
+// put all the ideas in the database
+async function putIdeas() {
+  console.log("putting ideas in the database");
+  ideas.forEach(async (idea) => {
+    idea = titleCase(idea);
+    const data = {
+      idea: idea,
+      description: "This is a description",
+    };
+    const newIdea = new ideaSchema(data);
+    await newIdea.save();
+  });
+  return "done";
+}
+
+async function deleteIdeas() {
+  console.log("deleting ideas from the database");
+  await ideaSchema.deleteMany({});
+  return "done";
+}
+
+// Fetch a radnom idea from database
+async function getRandomIdea() {
+  const count = await ideaSchema.countDocuments();
+  const random = Math.floor(Math.random() * count);
+  const idea = await ideaSchema.findOne().skip(random);
+  return idea;
+}
